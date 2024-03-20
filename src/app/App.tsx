2ca30fece2bab99ee25fbecb58aa8e2dc735d6fd
Aleksey <aleksei.travnov@mail.ru>
2024-03-20 12:50:00 +0400

    feat: add redux persist

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,20 +4,23 @@ import { MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
 import type react from 'react';
 import { Provider } from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 
-import { store } from '@/app/providers/StoreProvider/config/store';
+import { store, persister } from '@/app/providers/StoreProvider/config/store';
 import RootRouter from '@/app/RootRouter';
 
 const App: react.FC = () => {
   return (
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persister}>
       <BrowserRouter>
         <MantineProvider defaultColorScheme="dark">
           <Notifications position="top-right" />
           <RootRouter />
         </MantineProvider>
       </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 };
--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,16 +1,44 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage' 
 
 import curentUserSlice from '@/enteties/session/User/model/slice';
 import authenticationFormSlice from '@/features/Authentication/model/slice';
 import findUsersSlice from '@/features/UserSearchAutocomplete/model/slice';
 
+const rootReducer = combineReducers({
+  authenticationFormSlice: authenticationFormSlice,
+  curentUserSlice: curentUserSlice,
+  findUsersSlice: findUsersSlice
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist:['findUsersSlice'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    authenticationFormSlice: authenticationFormSlice,
-    curentUserSlice: curentUserSlice,
-    findUsersSlice: findUsersSlice
-  }
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
+export const persister = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>;
 
